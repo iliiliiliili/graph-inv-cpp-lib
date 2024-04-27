22d602805d22a1aa2f49f5f5d5f913ad3e637bd7
Illia Oleksiienko <io@ece.au.dk>
2024-04-27 15:23:47 +0200

    Implement decaying max heap

--- includes/ginv/clustering/decaying_max_heap.hpp
+++ includes/ginv/clustering/decaying_max_heap.hpp
@@ -1,6 +1,7 @@
 #ifndef DECAYING_MAX_HEAP_HPP_
 #define DECAYING_MAX_HEAP_HPP_
 
+#include <map>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -12,11 +13,13 @@ template <
     typename... TKeys>
 class DecayingMaxHeap {
     typedef std::tuple<TKeys...> TKeysTuple;
+    typedef std::tuple<TKeys..., TValue> TKeysValueTuple;
     typedef std::tuple<std::vector<TKeys>...> TKeysVectorTuple;
 
 public:
     TKeysVectorTuple m_heap_keys;
     std::vector<TValue> m_heap_values;
+    std::map<TKeysTuple, size_t> m_node_position_map;
 
     explicit DecayingMaxHeap(size_t size)
     {
@@ -34,6 +37,118 @@ public:
             m_heap_keys);
 
         m_heap_values.push_back(value);
+
+        siftdown(0, size() - 1, value, std::make_tuple(keys...));
+    }
+
+    void push(TKeysTuple keys_tuple, TValue value)
+    {
+
+        std::apply([&](auto&... keys) { push(keys..., value); },
+            keys_tuple);
+    }
+
+    void push(TKeysValueTuple keys_value_tuple)
+    {
+
+        std::apply([&](auto&... key_values) { push(key_values...); },
+            keys_value_tuple);
+    }
+
+    TKeysTuple top_key()
+    {
+        return key_at(0);
+    }
+
+    TValue top_value()
+    {
+        return value_at(0);
+    }
+
+    TKeysValueTuple top()
+    {
+        return std::apply([&](auto&... vectors) { return std::make_tuple(vectors[0]..., m_heap_values[0]); },
+            m_heap_keys);
+    }
+
+    TKeysValueTuple pop()
+    {
+
+        size_t latest_id = size() - 1;
+        TKeysTuple result_key = top_key();
+        TKeysValueTuple result = top();
+
+        remove_node_position_in_map(result_key);
+
+        if (latest_id > 0) {
+
+            assign(0, latest_id);
+
+            reduce_size();
+
+            siftup(0, size(), top_value(), top_key());
+        } else {
+
+            reduce_size();
+        }
+
+        return result;
+    }
+
+    TValue remove(TKeysTuple keys)
+    {
+
+        size_t latest_id = size() - 1;
+        size_t position = get_node_position_in_map(keys);
+        TKeysTuple output_key = key_at(position);
+        TValue value = value_at(position);
+
+        remove_node_position_in_map(keys);
+
+        if (latest_id > 0) {
+
+            assign(position, latest_id);
+
+            reduce_size();
+
+            siftup(position, size(), value_at(position), key_at(position));
+        } else {
+
+            reduce_size();
+        }
+    }
+    TValue remove(TKeys... keys)
+    {
+
+        return remove(std::make_tuple(keys...));
+    }
+
+    void update_value(TKeysTuple keys, TValue new_value)
+    {
+
+        size_t position = get_node_position_in_map(keys);
+        TValue old_value = value_at(position);
+        m_heap_values[position] = new_value;
+
+        if (new_value > old_value) {
+
+            siftdown(0, position, new_value, keys);
+        } else {
+
+            siftup(position, size() - 1, new_value, keys);
+        }
+    }
+
+    void update_value(TKeys... keys, TValue new_value)
+    {
+
+        update_value(std::make_tuple(keys...), new_value);
+    }
+
+    size_t size()
+    {
+
+        return m_heap_values.size();
     }
 
     std::string describe() const
@@ -42,7 +157,7 @@ public:
         return "DecayingMaxHeap(" + std::to_string(std::tuple_size<TKeysVectorTuple>()) + " keys and a value of " + std::to_string(m_heap_values.size()) + " heap elements)";
     }
 
-    std::string show_element(size_t element_id) const
+    std::string describe_element(size_t element_id) const
     {
 
         TKeysTuple keys = std::apply([element_id](auto&... vectors) { return std::make_tuple(vectors[element_id]...); },
@@ -54,13 +169,120 @@ public:
         return "DecayingMaxHeapElement@" + std::to_string(element_id) + "(keys " + keys_str + "and a value of " + std::to_string(m_heap_values[element_id]) + ")";
     }
 
-private:
-    void assign(size_t target, size_t source)
+    // private:
+    TKeysTuple key_at(size_t element_id)
     {
+
+        return std::apply([element_id](auto&... vectors) { return std::make_tuple(vectors[element_id]...); },
+            m_heap_keys);
     }
 
-    void assign_value(size_t target, TKeys... keys, TValue value)
+    TValue value_at(size_t element_id)
     {
+
+        return m_heap_values[element_id];
+    }
+
+    void assign(size_t target, size_t source)
+    {
+        m_heap_values[target] = m_heap_values[source];
+
+        [&]<std::size_t... Is>(std::index_sequence<Is...>) {
+            ([&]() {
+                auto& vector = std::get<Is>(m_heap_keys);
+                vector[target] = vector[source];
+            }(),
+                ...);
+        }(std::index_sequence_for<TKeys...>());
+
+        set_node_position_in_map(key_at(target), target);
+    }
+
+    void assign_value(size_t target, TValue value, TKeysTuple key)
+    {
+
+        m_heap_values[target] = value;
+
+        [&]<std::size_t... Is>(std::index_sequence<Is...>) {
+            ([&]() {
+                auto& vector = std::get<Is>(m_heap_keys);
+                auto& val = std::get<Is>(key);
+                vector[target] = val;
+            }(),
+                ...);
+        }(std::index_sequence_for<TKeys...>());
+
+        set_node_position_in_map(key, target);
+    }
+
+    void siftdown(size_t start, size_t position, TValue new_value, TKeysTuple new_key)
+    {
+
+        while (position > start) {
+
+            size_t parent = (position - 1) >> 1;
+            TValue parent_value = value_at(parent);
+
+            if (new_value > parent_value) {
+                assign(position, parent);
+                position = parent;
+            } else {
+
+                break;
+            }
+        }
+
+        assign_value(position, new_value, new_key);
+    }
+
+    void siftup(size_t position, size_t end, TValue new_value, TKeysTuple new_key)
+    {
+        size_t start = position;
+
+        size_t child_position = 2 * position + 1;
+
+        while (child_position < end) {
+
+            size_t right_child_position = child_position + 1;
+
+            if ((right_child_position < end) && !(m_heap_values[child_position] > m_heap_values[right_child_position])) {
+                child_position = right_child_position;
+            }
+
+            assign(position, child_position);
+            position = child_position;
+            child_position = 2 * position + 1;
+        }
+
+        assign_value(position, new_value, new_key);
+
+        siftdown(start, position, new_value, new_key);
+    }
+
+    void remove_node_position_in_map(TKeysTuple key)
+    {
+        m_node_position_map.erase(key);
+    }
+
+    size_t get_node_position_in_map(TKeysTuple key)
+    {
+        return m_node_position_map[key];
+    }
+
+    void set_node_position_in_map(TKeysTuple key, size_t position)
+    {
+        m_node_position_map[key] = position;
+    }
+
+    void reduce_size()
+    {
+
+        m_heap_values.pop_back();
+
+        std::apply([](auto&... vectors) {
+            (vectors.pop_back(), ...);
+        },
+            m_heap_keys);
     }
 };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <osigma/ograph.hpp>
 #include <osigma/onodes.hpp>
 #include <vector>
+#include <tuple>
 
 int main(int argc, char** argv)
 {
@@ -14,10 +15,40 @@ int main(int argc, char** argv)
     std::cout << heap.describe() << std::endl;
 
     heap.push(10, 15, 4);
+    heap.push(1, 11, 12.22);
+    auto top_key = heap.top_key();
+    std::get<0>(top_key) = 1333;
+    heap.push(top_key, 17);
+    auto top = heap.top();
+    std::get<1>(top) = 1333;
+    std::get<2>(top) = 0.1;
+    heap.push(top);
 
-    std::cout << heap.show_element(0) << std::endl;
-    std::cout << heap.show_element(1) << std::endl;
-    std::cout << heap.show_element(2) << std::endl;
+    std::cout << heap.describe_element(0) << std::endl;
+    std::cout << heap.describe_element(1) << std::endl;
+    std::cout << heap.describe_element(2) << std::endl;
+    std::cout << heap.describe_element(3) << std::endl;
+    std::cout << heap.describe_element(4) << std::endl;
+
+    auto show = [](auto&... values) { return ((std::to_string(values) + ", ") + ...); };
+    auto pop_show = [&heap, &show]() {
+        
+        auto result = heap.pop();
+        std::cout<<std::apply(show, result)<<std::endl;
+        std::cout<<heap.size()<<std::endl;
+    };
+
+    pop_show();
+    pop_show();
+    pop_show();
+    pop_show();
+
+    std::cout << "-----------------" << std::endl;
+    std::cout << heap.describe_element(0) << std::endl;
+    std::cout << heap.describe_element(1) << std::endl;
+    std::cout << heap.describe_element(2) << std::endl;
+    std::cout << heap.describe_element(3) << std::endl;
+    std::cout << heap.describe_element(4) << std::endl;
 
     return 0;
 
